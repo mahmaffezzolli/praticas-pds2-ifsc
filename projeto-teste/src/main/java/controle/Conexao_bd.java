2bd0725e0362ec27c79d3f1304bf6cad13650564
maria <maria.m12@aluno.ifsc.edu.br>
2023-08-01 08:53:30 -0300

    aula1

---
+++ projeto-teste/src/main/java/controle/Conexao_bd.java
@@ -0,0 +1,43 @@
+package controle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao_bd {	
+		private static Connection conexao;
+		private static Conexao_bd instancia;
+		private static final String DATABASE = "nome-banco";
+		private static final String USER     = "root";
+		private static final String PSW      = "aluno";
+		
+		private Conexao_bd() {}
+		
+		public static Conexao_bd getInstacia() {
+			if (instancia == null) { 
+				instancia = new Conexao_bd(); 
+			}
+			return instancia;	
+		}
+		
+		public static Connection conectar() {
+			try {
+				conexao = DriverManager.getConnection("jdbc:mysql://localhost/"+ DATABASE + "?serverTimezone=UTC", USER, PSW);
+			} catch (Exception e) { 
+				e.printStackTrace(); 
+			}
+			return conexao;		
+		}
+		
+		public boolean fecharConexao() { 
+			try { 
+				conexao.close(); 
+			} 
+			catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+
+			return true;
+		}
+}
