b7dc4086c7ba97bdbe36fba51c25a928e18ff6f6
maria <mariaeduardamaffezzolli@gmail.com>
2023-08-15 10:23:29 -0300

    select

--- projeto-teste/src/main/java/controle/MovieDAO.java
+++ projeto-teste/src/main/java/controle/MovieDAO.java
@@ -2,12 +2,50 @@ package controle;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import modelo.Movie;
 
 public class MovieDAO {
 
+	
+	public ArrayList<Movie> listar(){
+		
+        Conexao_bd c = Conexao_bd.getInstancia();
+		
+		Connection con = c.conectar();
+		ArrayList<Movie> movies = new ArrayList();
+		
+		String query = "SELECT FROM * MOVIES";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+			int movieId = rs.getInt("id_movie");
+			String nomeMovie = rs.getString("titulo_movie");
+			
+			Movie m = new Movie();
+			m.setIdMovie(movieId);
+			m.setNomeMovie(nomeMovie);
+			
+			movies.add(m);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		c.fecharConexao();
+
+		
+		
+		return movies;
+	}
+	
 	public boolean inserir(Movie m) {
 		
 		Conexao_bd c = Conexao_bd.getInstancia();
